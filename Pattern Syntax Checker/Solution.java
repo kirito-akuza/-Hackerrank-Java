4700de0fb414e827f2e8e7b6858e8828089e6e7a
Trung Nghia Dinh <85362812+kirito-akuza@users.noreply.github.com>
2022-09-08 13:23:28 -0700

    Create Solution.java

---
+++ Pattern Syntax Checker/Solution.java
@@ -0,0 +1,26 @@
+package HackerRank;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        for(int i = 0; i < n; i++) {
+            try{
+                Pattern.compile(in.nextLine());
+                System.out.println("Valid");
+            }
+            catch(PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+        }
+        
+    }
+}
+
