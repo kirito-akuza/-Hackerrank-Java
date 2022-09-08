ba2a93d07c8b14b9218650ee92cd8d22a90d76ae
Trung Nghia Dinh <85362812+kirito-akuza@users.noreply.github.com>
2022-09-08 11:59:13 -0700

    Create Solution.java

---
+++ End-of-file/Solution.java
@@ -0,0 +1,18 @@
+package HackerRank;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = 0;
+        
+        while (in.hasNext()) {
+            ++n;
+            System.out.println(n + " " + in.nextLine());
+        }
+    }
+}
